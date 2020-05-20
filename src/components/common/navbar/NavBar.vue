eed49ec8a8ce5913cd2d29f41f2137a8664823d3
litongye <l2t7y@163.com>
2020-05-20 17:37:04 +0800

    导航组件

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="nav-bar">
+    <!-- 不要设置插槽的样式，设置插槽外面div的样式 -->
+    <div class="nav-left">
+      <slot name="nav-slot-left"></slot>
+    </div>
+    <div class="nav-center">
+      <slot name="nav-slot-center"></slot>
+    </div>
+    <div class="nav-right">
+      <slot name="nav-slot-right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex; /* 类似于流布局 */
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  /* box-shadow:为元素添加阴影 */
+  box-shadow: 0px 2px 2px rgba(100, 100, 100, 0.1);
+}
+.nav-left,
+.nav-right {
+  width: 60px;
+}
+.nav-center {
+  /* 左右各占60px,中间部分使用flex:1占据剩余部分 */
+  flex: 1;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="home">
-    <h3>首页</h3>
+    <nav-bar id="home-nav">
+      <div slot="nav-slot-center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from "../../components/common/navbar/NavBar";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components:{
+    NavBar
+  }
 };
 </script>
 
 <style>
+  #home-nav{
+    /* 引用base.css文件中--color-tint变量的值 */
+    background-color:var(--color-tint);
+    color:#FFFFFF
+  }
 </style>
 
